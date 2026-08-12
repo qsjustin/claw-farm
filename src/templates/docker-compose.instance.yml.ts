1d1313f3a6e2d075058b07b53594146d61179c10
qsjustin <qsjustin@gmail.com>
2026-08-13 00:44:11 +0800

    fix(#179): make sidecar image and identity instance-scoped

--- src/templates/docker-compose.instance.yml.ts
+++ src/templates/docker-compose.instance.yml.ts
@@ -145,7 +145,7 @@ export function buildInstanceCompose(opts: InstanceComposeOptions): string {
 
   const weixinSidecarService = enableWeixinSidecar ? `  weixin-sidecar:
     container_name: ${containerPrefix}-weixin
-    image: clawbay-bay-sidecar-weixin:latest
+    image: \${WEIXIN_SIDECAR_IMAGE:-clawbay-bay-sidecar-weixin:latest}
     user: "1000:1000"
     env_file:
       - ./${weixinEnvFile}
@@ -155,9 +155,6 @@ export function buildInstanceCompose(opts: InstanceComposeOptions): string {
       GATEWAY_INTERNAL_TOKEN: \${GATEWAY_INTERNAL_TOKEN:-gateway-dev-token}
       SIDECAR_CLAW_BAY_API_URL: http://claw-bay-api:3001
       WEIXIN_ENABLE_ILINK_TRANSPORT: \${WEIXIN_ENABLE_ILINK_TRANSPORT:-false}
-      WEIXIN_REQUIRE_BAY_CREDENTIAL: \${WEIXIN_REQUIRE_BAY_CREDENTIAL:-false}
-      WEIXIN_INSTANCE_ID: \${WEIXIN_INSTANCE_ID:?Set WEIXIN_INSTANCE_ID in instance.env}
-      WEIXIN_IDENTITY_GENERATION: \${WEIXIN_IDENTITY_GENERATION:-1}
       OPENCLAW_STATE_DIR: /data/openclaw
       SESSION_STORAGE_PATH: /data/weixin-sessions
       WEIXIN_HEALTH_CHECK_URL: http://claw-bay-api:3001/health
